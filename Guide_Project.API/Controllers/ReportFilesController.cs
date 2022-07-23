2c2401e2ff63d3b61b0c65032fa653a3494537aa
BayramGUN <bayram.gun635@gmail.com>
2022-07-23 18:19:48 +0300

    Reportfile Controller added

--- Guide_Project.API/Controllers/ReportFilesController.cs
+++ Guide_Project.API/Controllers/ReportFilesController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Guide_Project.Core.Services;
 using Guide_Project.Core.DTOs;
+using Guide_Project.Core.Models;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Guide_Project.API.Controllers;
@@ -12,5 +13,21 @@ namespace Guide_Project.API.Controllers;
 [ApiController]
 public class ReportFilesController : CustomController
 {
-    
+    private readonly IGenericService<ReportFile, ReportFileDto> _reportFileService;
+
+    public ReportFilesController(IGenericService<ReportFile, ReportFileDto> reportFileService)
+    {
+        _reportFileService = reportFileService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAllReportFiles()
+    {
+        return ActionResultInstance(await _reportFileService.GetAllAsync());
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteReportFile(int id)
+    {
+        return ActionResultInstance(await _reportFileService.RemoveById(id));
+    }
+
 }
